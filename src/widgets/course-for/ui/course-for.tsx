f41ca637b9052ebf313e1b052e24c9125d5c58e3
Olya <olapancisina1@gmail.com>
2024-07-11 16:10:45 +0200

    add course for block

--- src/widgets/course-for/ui/course-for.tsx
+++ src/widgets/course-for/ui/course-for.tsx
@@ -6,14 +6,29 @@ export default function CourseFor() {
 			<div className='container'>
 				<MainTitle text='ДАНИЙ КУРС ДЛЯ:' className='text-center mb-[100px]' />
 
-				<div className='flex justify-between'>
-					<div className='flex flex-col'>
-						<div></div>
-						<div></div>
+				<div className='flex justify-between m-auto'>
+					<div className='flex flex-col mr-[15px]'>
+						<div className='w-[550px] bg-[#e1a421] text-white px-[20px] py-[10px] rounded-2xl text-[30px] mb-[30px] h-[300px]'>
+							- експретів-початківців, які хочуть знайти своє місце в інфобізі:
+							знайти свій напрямок, мати прості і дієві інструменти для
+							заробітку на своїй експертності
+						</div>
+						<div className='w-[550px] bg-[#e1a421] text-white px-[20px] py-[10px] rounded-2xl text-[30px] h-[300px]'>
+							- експертів які мають багато дипломів, різнопланового досвіду і
+							навичок, але не знають що з чим робити і як заробляти на своїй
+							експретизі
+						</div>
 					</div>
-					<div className='flex flex-col'>
-						<div></div>
-						<div></div>
+					<div className='flex flex-col ml-[15px]'>
+						<div className='w-[550px] bg-[#e1a421] text-white px-[20px] py-[10px] rounded-2xl text-[30px] mb-[30px] h-[300px]'>
+							- для багатопрофільних експертів, які мають гарні результати в
+							різних нішах, але хочуть заробляти на своїх талантах - те що
+							найбільше подобається
+						</div>
+						<div className='w-[550px] bg-[#e1a421] text-white px-[20px] py-[10px] rounded-2xl text-[30px] h-[300px]'>
+							- для власників консалтингових компаній, які хочуть перейти в
+							онлайн і не знать з чого почати
+						</div>
 					</div>
 				</div>
 			</div>
