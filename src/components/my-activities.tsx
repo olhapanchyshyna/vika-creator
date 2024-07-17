8219818f1b1ab71f0eb01364b6aac6a407ccdd0c
Olya <olapancisina1@gmail.com>
2024-07-17 14:49:40 +0200

    adaptive block my activites

--- src/components/my-activities.tsx
+++ src/components/my-activities.tsx
@@ -4,56 +4,54 @@ const raleway = Raleway({ subsets: ['latin'] })
 
 export default function MyActivities() {
 	return (
-		<section className='2xl:py-[100px] py-[80px]'>
-			<div className='container'>
+		<section className='pt-[50px] pb-[20px] lg:py-[80px] 2xl:py-[100px] px-[20px] lg:px-[60px]'>
+			<div className='container p-0'>
 				<MainTitle text='Моя діяльність в цифрах' className='text-center' />
 
-				<div className='flex flex-col justify-between mt-[100px]'>
-					<div className='flex justify-between items-center'>
-						{/* 1 */}
-						<div className='max-w-[400px] w-[100%]'>
-							<div
-								className={`text-center 2xl:text-[100px] text-[70px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
-							>
-								8
-							</div>
-							<div className='text-center text-[28px] text-white leading-[normal]'>
-								років в сфері маркетингу
-							</div>
+				<div className='flex justify-around items-end xl:items-center flex-wrap mt-[50px] md:mt-[100px]'>
+					{/* 1 */}
+					<div className='lg:max-w-[400px] max-w-[345px] w-[100%] mb-[40px]'>
+						<div
+							className={`text-center 2xl:text-[100px] xl:text-[70px] text-[50px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
+						>
+							700 000
 						</div>
-						{/* 2 */}
-						<div className='2xl:max-w-[460px] max-w-[360px] w-[100%]'>
-							<div
-								className={`text-center 2xl:text-[100px] text-[70px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
-							>
-								700 000
-							</div>
-							<div className='text-center text-[28px] text-white leading-[normal]'>
-								грн оберту на місяць кращої воронки
-							</div>
-						</div>
-						{/* 3 */}
-						<div className='max-w-[320px] w-[100%]'>
-							<div
-								className={`text-center 2xl:text-[100px] text-[70px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
-							>
-								7
-							</div>
-							<div className='text-center text-[28px] text-white leading-[normal]'>
-								років <br /> навчаю і консультую
-							</div>
+						<div className='text-center xl:text-[28px] lg:text-[23px] text-[20px] text-white leading-[normal]'>
+							грн оберту на місяць кращої воронки
 						</div>
 					</div>
-					<div className='flex justify-center'>
-						<div className='max-w-[400px] w-[100%] mt-[70px]'>
-							<div
-								className={`text-center 2xl:text-[100px] text-[70px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
-							>
-								665 000
-							</div>
-							<div className='text-center text-[28px] text-white leading-[normal]'>
-								грн приніс останній запуск
-							</div>
+					{/* 2 */}
+					<div className='lg:max-w-[400px] max-w-[345px]  w-[100%] mb-[40px]'>
+						<div
+							className={`text-center 2xl:text-[100px] xl:text-[70px] text-[50px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
+						>
+							8
+						</div>
+						<div className='text-center xl:text-[28px] lg:text-[23px] text-[20px] text-white leading-[normal]'>
+							років в сфері маркетингу
+						</div>
+					</div>
+
+					{/* 3 */}
+					<div className='lg:max-w-[400px] max-w-[345px]  w-[100%] mb-[40px]'>
+						<div
+							className={`text-center 2xl:text-[100px] xl:text-[70px] text-[50px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
+						>
+							7
+						</div>
+						<div className='text-center xl:text-[28px] lg:text-[23px] text-[20px] text-white leading-[normal]'>
+							років <br /> навчаю і консультую
+						</div>
+					</div>
+					{/* 4 */}
+					<div className='lg:max-w-[400px] max-w-[345px] w-[100%] mb-[40px]'>
+						<div
+							className={`text-center 2xl:text-[100px] xl:text-[70px] text-[50px] font-bold leading-[normal] text-[#e1a421]  ${raleway.className}`}
+						>
+							665 000
+						</div>
+						<div className='text-center xl:text-[28px] lg:text-[23px] text-[20px] text-white leading-[normal]'>
+							грн приніс останній запуск
 						</div>
 					</div>
 				</div>
